1950c3050e5f65b26e914f9753ab60f5d5a9f666
ooovector <ooovector>
2023-01-15 20:46:45 +0300

    add charge grid center to create_grid for charge dispersion calculations

--- QCircuit.py
+++ QCircuit.py
@@ -34,17 +34,19 @@ class QVariable:
         self.charge_step = None
         self.nodeNo = None
 
-    def create_grid(self, nodeNo, phase_periods, centre=0):
+    def create_grid(self, nodeNo, phase_periods, centre=0, centre_charge=0):
         """
         Creates a discrete grid for wavefunction variables.
         :param nodeNo: number of discrete points on the grid
         :param phase_periods: number of 2pi intervals in the grid
+        :param centre: center of phase grid
+        :param centre_charge: centre of charge grid (offset charge equivalent)
         """
         self.variable_type = 'variable'
         minNode = np.round(-nodeNo/2)
-        maxNode = np.round(nodeNo/2)
+        maxNode = minNode + nodeNo
         self.phase_grid = np.linspace(-np.pi*phase_periods+centre, np.pi*phase_periods+centre, nodeNo, endpoint=False)
-        self.charge_grid = np.linspace(minNode/phase_periods, maxNode/phase_periods, nodeNo, endpoint=False)
+        self.charge_grid = np.linspace(minNode/phase_periods+centre_charge, maxNode/phase_periods+centre_charge, nodeNo, endpoint=False)
         self.phase_step = 2*np.pi*phase_periods/nodeNo
         self.charge_step = 1.0/phase_periods
         self.nodeNo = nodeNo
